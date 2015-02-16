88f424bd75c949e12327a925d30381a6a599d34a
Ayachi Gin <ayachigin@gmail.com>
2015-02-16 20:28:22 +0900

    Add events.ts

---
+++ app/ts/events.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="scripts/typings/jquery/jquery.d.ts" />
+'use strict';
+
+module TourabuEx.events {
+
+    export interface Bind {
+        (eventType: string, handler: (e: JQueryEventObject, a: any) => any): any;
+    }
+
+    export var bind: Bind = function bind(eventType, f) {
+        return $(TourabuEx).bind.apply($(TourabuEx), arguments);
+    }
+
+    export interface Unbind {
+        (eventType: string): void;
+    }
+
+    export var unbind: Unbind = function () {
+        return $(TourabuEx).unbind.apply($(TourabuEx), arguments);
+    }
+
+    export interface Trigger {
+        (eventType: string, any): any;
+    }
+
+    export var trigger: Trigger = function (eventType, o) {
+        return $(TourabuEx).trigger.apply($(TourabuEx), arguments);
+    }
+} 
