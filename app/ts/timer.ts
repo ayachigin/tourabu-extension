84b676806776c71b8d6e34332b0dfc7ce790e854
Ayachi Gin <ayachigin@gmail.com>
2015-02-19 11:47:56 +0900

    Add

---
+++ app/ts/timer.ts
@@ -0,0 +1,92 @@
+﻿'use strict';
+
+module TourabuEx.timer {
+    export interface TimerTask {
+        end: Date;
+        type: string;
+        callbackParam: any;
+    }
+
+    interface TimerTaskToSave {
+        end: string;
+        type: string;
+        callbackParam: any;
+    }
+
+    var timers: TimerTask[] = [];
+    var seconds: number = 0;
+
+    export function set(p: TimerTask) {
+        timers.push(p);
+        saveTimers();
+    }
+
+    export function cancel(f: (t: TimerTask) => boolean) {
+        timers.forEach((t, i, ts) => {
+            if (f(t)) {
+                TourabuEx.util.removeAt(ts, i);
+                saveTimers();
+                return;
+            }
+        });
+    }
+
+    function saveTimers() {
+        var timersToSave: TimerTaskToSave[];
+
+        timersToSave = timers.map((task) => {
+            return {
+                end: task.end.toString(),
+                type: task.type,
+                callbackParam: task.callbackParam
+            }
+        });
+
+        console.log('timers/save', timersToSave);
+        TourabuEx.storage.set({ 'timer_tasks': timersToSave });
+    }
+
+    function loadTimers() {
+        TourabuEx.storage.get('timer_task', function (tasks: TimerTaskToSave[]) {
+            tasks.forEach((task) => {
+                var d = new Date(task.end);
+                if (TourabuEx.util.isValid(d)) {
+                    timers.push({
+                        end: d,
+                        type: task.type,
+                        callbackParam: task.callbackParam
+                    });
+                }
+            });
+        })
+    }
+
+    function checkTimers() {
+        var now = new Date();
+        timers = timers.filter((task) => {
+            var ended = task.end < now;
+
+            TourabuEx.events.trigger('timer/' + task.type + '/end', task.callbackParam);
+
+            return ended;
+        });
+        saveTimers();
+    }
+
+    function initialize() {
+        var events = TourabuEx.events;
+
+        timers  = [];
+        seconds = 0;
+
+        loadTimers();
+
+        events.bind('second/change', function () {
+            if ((seconds++) % 5 === 0) {
+                checkTimers();
+            }
+        });
+    }
+
+    initialize();
+} 
