42b7aafde1baf0bb620a07e51df918d11bf9f5a8
Ayachi Gin <ayachigin@gmail.com>
2015-02-19 14:09:11 +0900

    Add widget.ts

---
+++ app/ts/widget.ts
@@ -0,0 +1,29 @@
+﻿module Tourabu.widget {
+    var widgetTab = null;
+
+    TourabuEx.events.bind('message/content/load', function (_, mes) {
+        console.log(mes);
+        chrome.windows.get(mes.sender.tab.windowId, function (w) {
+            console.log(w);
+            if (w.type === 'popup') {
+                widgetTab = mes.sender.tab;
+                chrome.tabs.sendMessage(mes.sender.tab.id, 'mode/widget');
+            }
+        });
+    });
+
+    TourabuEx.events.bind('message/start/widget', function () {
+        TourabuEx.util.startTourabuWidget();
+    });
+
+    /*
+    chrome.tabs.onZoomChange.addListener(function (o) {
+        if (widgetTab && widgetTab.id === o.tabId) {
+            chrome.tabs.sendMessage(widgetTab.id, {
+                type: 'zoom/change',
+                scale: o.newZoomFactor
+            });
+        }
+    });
+    */
+} 
