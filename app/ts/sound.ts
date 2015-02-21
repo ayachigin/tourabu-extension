1a7a3afe7f2e54338b2581d2cfa1718bbacf5d74
Ayachi Gin <ayachigin@gmail.com>
2015-02-21 17:53:40 +0900

    Add notification sound

---
+++ app/ts/sound.ts
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+module TourabuEx {
+    export class SEPlayer {
+        private players: HTMLAudioElement[];
+
+        /**
+        @param uls List of audio file's url
+        */
+        constructor(urls: Array<string>) {
+            this.players = [];
+            urls.forEach((url) => {
+                var filename = url.split('/').pop().split('.')[0];
+                this.players[filename] = new Audio(url);
+            })
+        }
+
+        /** play
+        @param  filename  file name that you want to play
+        */
+        play(filename: string) {
+            this.players[filename].play();
+        }
+    }
+} 
