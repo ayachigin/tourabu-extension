aee0d5c69f57b39e8699eddff895e93b537ecefa
Ayachi Gin <ayachigin@gmail.com>
2015-03-13 21:43:22 +0900

    Modify sstp Send sstp when events ocurred

--- app/ts/sstp.ts
+++ app/ts/sstp.ts
@@ -24,4 +24,40 @@ module TourabuEx.SSTP {
             }
         }
     }
+
+    TourabuEx.events.bind('conquest/start',(_, r) => {
+        var body = <TourabuEx.conquest.ConquestRequestBody> r.body,
+            party = body.party_no[0];
+
+        Functools.Maybe.pure(body.field_id[0])
+            .bind(function (fid) {
+            return TourabuEx.conquest.fieldId2Time(fid);
+        })
+            .bind(function (n) {
+            var c = new Client(),
+                min = Math.floor(n / 1000 / 60);
+
+            c.send('OnTourabuConquestStart,とうらぶえくすてんしょん,\\0第' + party +
+                '部隊が遠征を開始しました。\\n' + min +
+                '分後に帰還します。\\e,' + party + ',' + min);
+            return Functools.Maybe.Nothing();
+        });
+    });
+
+    TourabuEx.events.bind('timer/conquest/end',(_, p) => {
+        var c = new Client();
+
+        c.send('OnTourabuConquestEnd,とうらぶえくすてんしょん,\\0第' + p.party_no +
+            '部隊が遠征から帰還しました。,' + p.party_no + ',' + p.field_id);
+    });
+
+    TourabuEx.events.bind('duty/start',() => {
+        var c = new Client();
+        c.send('OnTourabuDutyStart,とうらぶえくすてんしょん,内番を開始しました。');
+    });
+
+    TourabuEx.events.bind('timer/duty/end',(_, r) => {
+        var c = new Client();
+        c.send('OnTourabuDutyStart,とうらぶえくすてんしょん,内番が終了しました。');
+    });
 }
