1de5a0623aa9a33f0691ab59fe88789bee05d27f
Ayachi Gin <ayachigin@gmail.com>
2015-03-13 19:55:20 +0900

    Add data type Maybe and tests for it

---
+++ app/ts/Functools.ts
@@ -0,0 +1,92 @@
+﻿module Functools {
+    function id<a>(v: a): a {
+        return v;
+    }
+
+    function _<a, b, c>(f1: (v: a) => b, f2: (v: b) => c): (a) => c {
+        return function (a) {
+            return f2(f1(a));
+        }
+    }
+
+    function join<a>(v: Monad<Monad<a>>): Monad<a> {
+        return v.bind(id);
+    } 
+
+    interface Functor<a> {
+        fmap<b>(f: (v: a) => b): Functor<b>;
+    }
+
+    interface Monad<a> extends Functor<a> {
+        pure(v: a): Monad<a>;
+        bind<b>(f: (v: a) => Monad<b>): Monad<b>;
+        then<b>(m: Monad<b>): Monad<b>;
+        fail(msg: string): Monad<a>;
+    }
+
+    export class Maybe<a> implements Monad<a> {
+
+        private hasValue: boolean;
+        private value: a;
+
+        constructor(v: a) {
+            if (v !== undefined &&
+                v !== null) {
+                this.hasValue = true;
+                this.value = v;
+            } else {
+                this.hasValue = false;
+            }
+        }
+
+        static Nothing<a>() {
+            return new Maybe<a>(null);
+        }
+
+        isJust() {
+            return this.hasValue;
+        }
+
+        isNothing() {
+            return !this.isJust();
+        }
+
+        maybe<b>(v: b, f: (w: a) => b): b {
+            return this.isJust() ? f(this.value) : v;
+        }
+
+        fromMaybe(v: a): a {
+            return this.isJust() ? this.value : v;
+        }
+
+        // instance Functor Maybe
+        fmap<b>(f: (v: a) => b) {
+            return this.isJust() ? new Maybe(f(this.value)) : this;
+        }
+
+        // instance Monad Maybe
+        static pure<a>(v: a) {
+            return new Maybe<a>(v);
+        }
+
+        pure(v: a) {
+            return Maybe.pure(v);
+        }
+
+        bind<b>(f: (v: a) => Monad<b>): Monad<b> {
+            if (this.isJust()) {
+                return f(this.value);
+            } else {
+                return Maybe.Nothing();
+            }
+        }
+
+        then<b>(m: Monad<b>): Monad<b> {
+            return m;
+        }
+
+        fail(msg: string): Monad<a> {
+            return Maybe.Nothing();
+        }
+    }
+} 
---
+++ test/spec/FunctoolsSpec.js
@@ -0,0 +1,111 @@
+/*global describe, it, Functools, expect */
+'use strict';
+
+(function () {
+    var throwError = function (){
+        throw new Error('AIEEEEEEEE');
+    };
+
+    var Maybe = Functools.Maybe;
+    
+    describe('Maybe a', function () {
+        describe ('Data', function () {
+            it('has a value if argument is not null or undefined', function () {
+                expect((new Maybe(1)).isJust()).toEqual(true);
+            });
+
+            it('not has a value if argument is null or undefined', function () {
+                expect((new Maybe(null)).isJust()).toEqual(false);
+                expect((new Maybe(undefined)).isNothing()).toEqual(true);
+            });
+        });
+
+        describe('Functor', function () {
+            it ('should possible to apply a function to the inner value', function () {
+                expect((new Maybe(1)).fmap(function (v) {
+                    return v * 2;
+                }).fromMaybe(null)).toEqual(2);
+
+                expect((new Maybe(1)).maybe("", function (n) {
+                    return '' + n;
+                })).toEqual('1');
+
+                expect((new Maybe(1)).fromMaybe(0)).toEqual(1);
+            });
+
+            it ('should be Nothing if apply a function to Nothing', function (){
+                expect((new Maybe(null)).fmap(throwError).isNothing()).toBeTruthy();
+
+                expect((new Maybe(undefined)).fmap(throwError).isNothing()).toBeTruthy();
+
+                expect((new Maybe(null)).maybe("a", throwError)).toEqual("a");
+
+                expect((new Maybe(undefined)).fromMaybe("a")).toEqual("a");
+            });
+
+            it ('Functor Maybe', function () {
+                expect((new Maybe(1)).fmap(function (n) {
+                    return n * 2;
+                }).isJust()).toBeTruthy();
+
+                expect (new Maybe(null).fmap(function (n) {
+                    return n * 2;
+                }).isJust()).toBeFalsy();
+            });
+        });
+
+        describe('Applicative Maybe', function() {
+            it('can bind a function', function () {
+                function evenDouble (n) { return n%2 === 0? Maybe.pure(n*2): Maybe.Nothing();}
+                Maybe.pure(1)
+                    .bind(function (n) {
+                        return Maybe.pure(n * 2);
+                    })
+                    .bind(function (n) {
+                        expect(n).toEqual(2);
+                    });
+
+                Maybe.pure(2)
+                    .bind(evenDouble)
+                    .bind(function (n) {
+                        expect(n).toEqual(4);
+                    });
+
+                Maybe.pure(1)
+                    .bind(evenDouble)
+                    .bind(function (n) {
+                        return Maybe.pure(expect(n.isNothing()).toBeTruthy());
+                    });
+            });
+        });
+
+        describe('Monad Maybe', function() {
+            it('puts it in a minimal context', function () {
+                expect(Maybe.pure(1).fromMaybe(null)).toEqual(1);
+            });
+            
+            it('can bind a function', function () {
+                function evenDouble (n) { return n%2 === 0? Maybe.pure(n*2): Maybe.Nothing();}
+                Maybe.pure(1)
+                    .bind(function (n) {
+                        return Maybe.pure(n * 2);
+                    })
+                    .bind(function (n) {
+                        expect(n).toEqual(2);
+                    });
+
+                Maybe.pure(2)
+                    .bind(evenDouble)
+                    .bind(function (n) {
+                        expect(n).toEqual(4);
+                    });
+
+                Maybe.pure(1)
+                    .bind(evenDouble)
+                    .bind(function (n) {
+                        return Maybe.pure(expect(n.isNothing()).toBeTruthy());
+                    });
+            });
+        });
+    });
+})();
